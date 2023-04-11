1e23f212e545105467e2f9eb2a4b137fffc78289
Alister Smith <alister.psmith@gmail.com>
2023-04-11 16:04:02 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alister on 11/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
